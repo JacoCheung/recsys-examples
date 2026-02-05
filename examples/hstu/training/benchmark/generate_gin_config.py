8d3f295c283f72ccdda718c60e941191be762e7b
JacoCheung <gloomy955@163.com>
2026-02-05 10:29:26 +0000

    Set large enough capacity for user_d

--- examples/hstu/training/benchmark/generate_gin_config.py
+++ examples/hstu/training/benchmark/generate_gin_config.py
@@ -113,7 +113,7 @@ action_embedding/EmbeddingArgs.sharding_type = 'data_parallel'
 # Contextual Feature Embeddings (3 total)
 user_id_emb/DynamicEmbeddingArgs.feature_names = ['user_id']
 user_id_emb/DynamicEmbeddingArgs.table_name = 'user_id'
-user_id_emb/DynamicEmbeddingArgs.item_vocab_size_or_capacity = 10000  # 10K (smaller than item)
+user_id_emb/DynamicEmbeddingArgs.item_vocab_size_or_capacity = 50000000  # 50M
 user_id_emb/DynamicEmbeddingArgs.item_vocab_gpu_capacity_ratio = {ratio}
 user_id_emb/DynamicEmbeddingArgs.evict_strategy = '{evict}'
 user_id_emb/DynamicEmbeddingArgs.caching = {caching}
